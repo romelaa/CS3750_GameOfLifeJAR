38772327725d77a244ad721b93412faed6d5220f
romelaa <34079912+romelaa@users.noreply.github.com>
2020-04-03 15:28:36 -0600

    Chat.js prints hello when clicking on a cell

--- GameOfLife/GameOfLife/wwwroot/js/chat.js
+++ GameOfLife/GameOfLife/wwwroot/js/chat.js
@@ -2,8 +2,59 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-//Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
+
+connection.start().then(function () {
+    console.log("Connected.");
+    document.getElementById("sendButton").disabled = false;
+    drawBox();
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+let canvas = document.getElementById("canvas"), c = canvas.getContext("2d");
+let numOfCells = 16;
+let canvasWH = 800;
+
+canvas.addEventListener('click', handleClick);
+
+function drawBox() {
+    c.beginPath();
+    c.fillStyle = "white";
+    c.lineWidth = 3;
+    c.strokeStyle = 'black';
+    for (var row = 0; row < 16; row++) {
+        for (var column = 0; column < 16; column++) {
+            var x = column * 50;
+            var y = row * 50;
+            c.rect(x, y, 50, 50);
+            c.fill();
+            c.stroke();
+        }
+    }
+    c.closePath();
+}
+
+function handleClick(e) {
+    var selectedColor = document.getElementById("selectedColor").value;
+    c.fillStyle = "#" + selectedColor;
+
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    c.fillRect(Math.floor(x / 50) * 50,
+        Math.floor(y / 50) * 50,
+        48, 48);
+
+    var cellNum = (x / 50) + (y / 50);
+
+    var li = document.createElement("li");
+    li.textContent = "hello from  cell " + cellNum;
+    document.getElementById("messagesList").appendChild(li);
+
+    connection.invoke("NewCells", x, y);
+}
+
+
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -11,13 +62,10 @@ connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
+
+
 });
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
